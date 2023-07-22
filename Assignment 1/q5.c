70d18ff1e450215786d8e1232f5047e9763a0841
karansagar486 <113330400+karansagar486@users.noreply.github.com>
2023-07-22 13:10:42 +0530

    yes

---
+++ Assignment 1/q5.c
@@ -0,0 +1,8 @@
+// WAP to calculate the length of String using printf function.
+#include <stdio.h>
+int main() {
+    //  doubt solved
+    int length = printf("Bidya");
+    printf("\nLength of the string is %d", length);
+    return 0;
+}
